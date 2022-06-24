7d8b38e92199df6338d3f253794dfdcb6fa61bf5
Dreamy <92980874+D134my@users.noreply.github.com>
2022-06-24 19:30:08 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
   hafez h;
 
   // h.set_token(your token);
-  // first of first create a mysql database and import faals.sql
+  // first of first create a mysql database then import faals.sql
   // h.set_db("db_username", "your password", " db_name for example hafez_discord");
   h.start();
 
